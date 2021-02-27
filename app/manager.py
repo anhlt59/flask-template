8b5a804f2e76dd3adf7ab0c46af2541b821a189f
AnhLT <anhlt@fabbi.io>
2021-02-27 18:13:18 +0700

    UPDATE

--- app/manager.py
+++ app/manager.py
@@ -3,18 +3,18 @@ import os
 from flask import Flask
 
 # load environment variables
-APP_SETTINGS = os.environ.get('APP_SETTINGS')
-PORT = os.environ.get('PORT', 5000)
-NAME = os.environ.get('APP_NAME', __name__)
+APP_SETTINGS = os.environ.get("APP_SETTINGS")
+PORT = os.environ.get("PORT", 5000)
+NAME = os.environ.get("APP_NAME", __name__)
 
 app = Flask(NAME)
 app.config.from_object(APP_SETTINGS)
 
 
-@app.route('/')
+@app.route("/")
 def hello():
-    return 'Hello World!'
+    return "Hello World!"
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(port=PORT)
